0f7a2eee113912d119e10b396320c17565003f8b
Enzo Neto <enzocasn@al.insper.edu.br>
2019-11-25 11:41:20 -0300

    fotos

--- Joguinho/selecao_minigame.py
+++ Joguinho/selecao_minigame.py
@@ -69,7 +69,7 @@ def selecao_minigame(screen):
     botoes = pygame.sprite.Group()
     
 
-    background = pygame.image.load(path.join(img_dir, 'back.jpg')).convert()
+    background = pygame.image.load(path.join(img_dir, 'back.png')).convert()
     background = pygame.transform.scale(background, (WIDTH, HEIGHT))
     background_rect = background.get_rect()
     Y_ = 10
